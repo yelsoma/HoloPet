fdad49731728c5f85ee29365b98c490170fbb92f
yelsoma <ivan.lin0406@gmail.com>
2025-06-26 21:46:16 +0800

    add SM maker

--- HoloPet/Assets/2.Scripts/Tools/SMPackage.cs
+++ HoloPet/Assets/2.Scripts/Tools/SMPackage.cs
@@ -4,4 +4,37 @@ using UnityEngine;
 
 public class SMPackage 
 {
+    //Basic
+    #region Basic St
+    #endregion
+    #region Basic Mg
+    #endregion
+
+    //RandomMove
+    #region RandomMove Mg
+    #endregion
+
+    //Mountable
+    #region Mountable Mg
+    #endregion
+
+    //MountingAbility
+    #region MountingAbility St
+    #endregion
+    #region MountingAbility Mg 
+    #endregion
+
+    //Interactable
+    #region Interactable Mg 
+    #endregion
+
+    //InteractAbility
+    #region InteractAbility St
+    #endregion
+    #region InteractAbility Mg
+    #endregion
+
+    //empty
+    #region empty
+    #endregion
 }
---
+++ HoloPet/Assets/2.Scripts/Tools/StateMachineMaker.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachineMaker : MonoBehaviour
+{
+    [SerializeField] StateMachineBase stateMachineBase;
+
+    [ContextMenu("Check Components & Auto-Fill")]
+
+    private void CheckAndAutoFill()
+    {
+        
+    }
+}
